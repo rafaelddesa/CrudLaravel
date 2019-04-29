daea810c64b227362cdb184daffdbf38ca252128
RafaelRodriguesdeSa <rafael.rodriguesdesa@hotmail.com>
2019-04-29 18:43:52 -0300

    Pagina App crud

---
+++ app/Curso.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    protected $fillable = [
+        'titulo','descricao','valor','imagem','publicado'
+    ];
+
+}
